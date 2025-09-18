fc79b8ecba6e565aedbe399ced332075ef6879b1
swaraj150 <swaraj.andhale@vit.edu.in>
2025-09-18 22:17:45 +0530

    refactor(sweet): refactor SweetRepository to add update method

--- server/src/main/java/com/sweetshop/server/repository/SweetRepository.java
+++ server/src/main/java/com/sweetshop/server/repository/SweetRepository.java
@@ -1,10 +1,15 @@
 package com.sweetshop.server.repository;
 
 import com.sweetshop.server.entity.Sweet;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Repository
-public interface SweetRepository extends CrudRepository<Sweet,Long> {
+public interface SweetRepository extends JpaRepository<Sweet,Long> {
+
+    @Query("UPDATE Sweet SET stockCount=:stock WHERE id=:id")
+    void updateStockCount(@RequestParam Long id, @RequestParam Integer stock);
 
 }
