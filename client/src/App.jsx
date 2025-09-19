67ae55495bcdd269848feabe1f9a2cb0c24e89ec
swaraj150 <swaraj.andhale@vit.edu.in>
2025-09-19 17:17:53 +0530

    feat(UI): add react router dom and chakra ui wrapper components with routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,33 +2,38 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { ChakraProvider } from "@chakra-ui/react";
+import SignUpPage from './components/SignupPage';
+import { publicRoutes } from './routes/Routes';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
+  const handleSignIn = (data) => {
+    console.log("Sign In:", data);
+    // call backend login API here
+  };
 
+  const handleSignUp = (data) => {
+    console.log("Sign Up:", data);
+    // call backend register API here
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+      <ChakraProvider>
+        <BrowserRouter>
+          <Routes>
+            {
+            publicRoutes.map((route, index) => (
+              <Route path={route.path} key={index} element={route.element} />
+            ))
+          }
+          </Routes>
+          
+        
+        </BrowserRouter>
+      </ChakraProvider>
+
+    
   )
 }
 
