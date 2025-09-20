1a253bcfc4113a2c4926cd65fb8fcb7dde320bb8
swaraj150 <swaraj.andhale@vit.edu.in>
2025-09-20 17:54:22 +0530

    refactor(security):add cors origin

--- server/src/main/java/com/sweetshop/server/config/SecurityConfig.java
+++ server/src/main/java/com/sweetshop/server/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
