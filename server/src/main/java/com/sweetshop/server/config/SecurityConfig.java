ff6d85fd6d5ed9d70c70bcde82fcb2308dcde74e
swaraj150 <swaraj.andhale@vit.edu.in>
2025-09-22 01:15:56 +0530

    refactor(security): add deployed frontend url

--- server/src/main/java/com/sweetshop/server/config/SecurityConfig.java
+++ server/src/main/java/com/sweetshop/server/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://tdd-kata-sweet-shop-management-system-1.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
